5152784be873d604add9659d3da422e8150dd044
Huseyn Garayev <huseyn@email.com>
2022-03-27 23:32:39 +0300

    ss

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,9 +1,19 @@
+import React, { createContext, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 const AppReducer = (state, action) => {
 	switch (action.type) {
 		case 'ADD_EXPENSE':
 			return {
 				...state,
 				expenses: [...state.expenses, action.payload],
+			};
+		case 'DELETE_EXPENSE':
+			return {
+				...state,
+				expenses: state.expenses.filter(
+					(expense) => expense.id !== action.payload
+				),
 			};
 		default:
 			return state;
